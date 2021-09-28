41a0e55698523941eadcbd6ea8d86e75d7046510
alimasanov <alimasanov.u@gmail.com>
2021-09-28 16:02:23 +0300

    fix dark mode

--- app/src/main/java/com/my/cryptocurrency/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/my/cryptocurrency/presentation/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.my.cryptocurrency.presentation.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -21,7 +22,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CryptocurrencyAppYTTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+fun CryptocurrencyAppYTTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
